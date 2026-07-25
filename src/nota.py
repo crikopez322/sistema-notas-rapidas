bd33fa1526f1b514b12976e898c99cc5ff30d44a
crikopez322 <crikopez@gmail.com>
2026-07-25 11:13:48 -0600

    Creada la clase base Nota con atributos e id

--- src/nota.py
+++ src/nota.py
@@ -1,6 +1,8 @@
-#clase para presentar una nota xdd
-class nota:
-    def __init__(self, titulo, contenido):
+import datetime #esto es una libreria que nos permite trabajar con fechas y horas xd 
+
+class Nota: 
+    def __init__(self, id_nota, titulo, contenido):
+        self.id_nota = id_nota
         self.titulo = titulo
         self.contenido = contenido
-
+        self.fecha_creacion = datetime.datetime.now().strftime("%Y-%m- %d %H:%M")
