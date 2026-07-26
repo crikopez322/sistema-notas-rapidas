3f386fd78117f2ad5bb95515e430278cbc2cc302
crikopez322 <crikopez@gmail.com>
2026-07-26 09:55:14 -0600

    principio de validaciones para que se puedan guiar

--- src/validaciones.py
+++ src/validaciones.py
@@ -1,3 +1,16 @@
 #validaciones de entrada de datos xd
-def validar_texto(texto):
-    return len(texto.strip()) > 0
+def validar_texto_no_vacio(mensaje):
+    while True:
+        texto = input(mensaje).strip()
+        if texto: #si rl texto tiene algo 
+            return texto #devuelve el texto
+        print("no puedes dejar este campo vacio bro")#por si el usuario deja bacio el camopo
+
+        def validar_numero_entero(mensaje): #valida la entrada de un entero
+            while True:
+                entrada = input(mensaje).strip() #pde el doto
+                if entrada.isdigit(): #checa si es numero 
+                    return int(entrada) #devuelve el numero entero lo regresa
+                print("introduce un numero valido bro, nada de letras o cosas raras") #por si al usuario le falla xdddd
+                
+    
